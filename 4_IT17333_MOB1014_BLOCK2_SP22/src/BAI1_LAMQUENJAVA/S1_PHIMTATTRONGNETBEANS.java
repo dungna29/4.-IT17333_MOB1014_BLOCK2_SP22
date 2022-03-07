cfc5a9f3af68b74d8fca7bdcd41070f44b07d03c
dungna29 <dungna29@localhost>
2022-03-07 17:50:12 +0700

    Dungna 2022

---
+++ 4_IT17333_MOB1014_BLOCK2_SP22/src/BAI1_LAMQUENJAVA/S1_PHIMTATTRONGNETBEANS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BAI1_LAMQUENJAVA;
+
+/**
+ *
+ * @author dungna29
+ */
+public class S1_PHIMTATTRONGNETBEANS {
+    //Các phím tắt trong netbeans
+    public static void main(String[] args) {
+        System.out.println("CHào các bạn đến với java ");
+    }
+}
