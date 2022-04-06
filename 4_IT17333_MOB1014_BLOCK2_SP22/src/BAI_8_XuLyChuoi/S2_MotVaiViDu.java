9e80e33892002422b9840f4b4fd530089b2ae9a1
dungna29 <dungna29@localhost>
2022-04-06 18:08:42 +0700

    Dungna 2022

--- 4_IT17333_MOB1014_BLOCK2_SP22/src/BAI_8_XuLyChuoi/S2_MotVaiViDu.java
+++ 4_IT17333_MOB1014_BLOCK2_SP22/src/BAI_8_XuLyChuoi/S2_MotVaiViDu.java
@@ -11,9 +11,41 @@ import java.util.Scanner;
  * @author dungna29
  */
 public class S2_MotVaiViDu {
-    //Bài 1: Viết 1 chương trình nhập vào Full Name sao đó viết hoa chữ cái đầu của từ lên.  nguyen thi hoa = Nguyen Thi Hoa
+    //Bài 1: Viết 1 chương trình nhập vào Full Name sao đó viết hoa chữ cái đầu của từ lên.  nguyEn tHi hoa = Nguyen Thi Hoa
     //Bài 2: Viết 1 chương trình zen mã tự động như sinh viên tại FPOLY.  nguyễn thị hoa = HoaNTPHXXX
     //Giải quyết 2 bài trên dựa vào lý thuyết xử lý chuỗi thầy đã up.
-    
    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        do {
+            String input;
+            System.out.print("Mời bạn nhập đầy đủ họ tên: ");
+            input = sc.nextLine();
+            System.out.printf("Tên bạn: %s \n",vietHoaChuCaiDauFullName(input));
+            System.out.printf("MSV bạn: %s \n",zenMSVFpoly(input,"012999"));
+        } while (true);
+        
+    }
+    public static String zenMSVFpoly(String text, String number){//nguyen thi hoa
+        String temp = text.trim().toLowerCase();
+        String arrNames[] = temp.split("\\s+");//Tách chuỗi thành mảng theo điều kiện
+        String msv =vietHoaChuCaiDauCuaTu(arrNames[arrNames.length-1]);//Hoa
+        for (int i = 0; i < arrNames.length-1; i++) {
+            msv+=String.valueOf(arrNames[i].charAt(0)).toUpperCase();
+        }
+        return msv +"PH"+number;
+    }
+    public static String vietHoaChuCaiDauFullName(String text){//nguyen thi hoa
+        String temp = text.trim().toLowerCase();
+        String arrNames[] = temp.split("\\s+");//Tách chuỗi thành mảng theo điều kiện
+        String fullName = "";
+        for (String x : arrNames) {
+            fullName+= vietHoaChuCaiDauCuaTu(x) +" ";
+        }
+        return fullName;
+    }
+    public static String vietHoaChuCaiDauCuaTu(String text){//nGUYEN
+        String temp = text.trim().toLowerCase();//nguyen
+        return String.valueOf(temp.charAt(0)).toUpperCase()+temp.substring(1);//Nguyen
+    }
 }
--- 4_IT17333_MOB1014_BLOCK2_SP22/src/BAI_8_XuLyChuoi/S4_SoSanh.java
+++ 4_IT17333_MOB1014_BLOCK2_SP22/src/BAI_8_XuLyChuoi/S4_SoSanh.java
@@ -9,8 +9,26 @@ package BAI_8_XuLyChuoi;
  * @author dungna29
  */
 public class S4_SoSanh {
+
     public static void main(String[] args) {
-        String[] arrName = {"abc","Abe","aba","Ade"};
-       
+        String[] arrName = {"abc", "ABC", "aba", "Adae"};
+        for (String x : arrName) {
+            if (x.contains("a")) {
+                System.out.print(x + " ");
+            }
+        }
+        System.out.println("");
+        for (String x : arrName) {
+            if (x.endsWith("a")) {
+                System.out.print(x + " ");
+            }
+        }
+        System.out.println("");
+        for (String x : arrName) {
+            if (x.equalsIgnoreCase("abc")) {
+                System.out.print(x + " ");
+            }
+        }
+        System.out.println("");
     }
 }
