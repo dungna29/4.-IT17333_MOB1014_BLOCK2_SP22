601ef4bf3b327608bbbbac76d7c0d8b8df03eeca
dungna29 <dungna29@localhost>
2022-04-06 16:26:56 +0700

    Dungna 2022

---
+++ 4_IT17333_MOB1014_BLOCK2_SP22/src/BAI_8_XuLyChuoi/S4_SoSanh.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BAI_8_XuLyChuoi;
+
+/**
+ *
+ * @author dungna29
+ */
+public class S4_SoSanh {
+    public static void main(String[] args) {
+        String[] arrName = {"abc","Abe","aba","Ade"};
+       
+    }
+}
